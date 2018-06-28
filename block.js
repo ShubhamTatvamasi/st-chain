9ea0031887e36b4bd12a2fcc1280d91e27a8d473
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-06-28 20:59:10 +0530

    modified:   block.js

--- block.js
+++ block.js
@@ -9,14 +9,14 @@ class Block {
 
   toString() {
     return `Block - 
-    Timestamp: ${this.timestamp.substring(0, 10)}
+    Timestamp: ${this.timestamp}
     Last Hash: ${this.lasthash.substring(0, 10)}
     Hash     : ${this.hash.substring(0, 10)}
-    Data     : ${this.data.substring(0, 10)}`
+    Data     : ${this.data}`
   }
 
   static genesis() {
-    return new this('Genesis time', '-----', 'f1r57-h45h', '')
+    return new this('Genesis time', '-----', 'f1r57-h45h', [])
   }
 
 }
