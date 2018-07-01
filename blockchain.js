ed7f6d526454f00f047f6e3461a28952bbe51249
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-07-01 11:56:08 +0530

    modified:   blockchain.js 	new file:   blockchain.test.js

--- blockchain.js
+++ blockchain.js
@@ -6,8 +6,9 @@ class Blockchain {
     this.chain = [Block.genesis()]
   }
 
-  addBlock() {
-    const block = Block.mineBlock(this.chain[this.chain-length-1], date)
+  addBlock(data) {
+    const lastBlock = this.chain[this.chain.length-1]
+    const block = Block.mineBlock(lastBlock, data)
     this.chain.push(block)
 
     return block
