db1e9eb04e105c8cd550895a2228e158a59710aa
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-07-02 10:21:00 +0530

    modified:   app/index.js 	modified:   app/p2p-server.js

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -1,6 +1,6 @@
 const Websocket = require('ws')
 
-const P2P_PORT = process.env.P2P_PORT || 5001
+const P2P_PORT = process.env.P2P_PORT || 5000
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : []
 
 class P2pSperver {
@@ -12,7 +12,18 @@ class P2pSperver {
   listen() {
     const server = new Websocket.Server({port: P2P_PORT})
     server.on('connection', socket => this.connectSocket(socket))
+
+    this.connectToPeers()
+
     console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`)
+  }
+
+  connectToPeers() {
+    peers.forEach(peer => {
+      const socket = new Websocket(peer)
+
+      socket.on('open', () => this.connectSocket(socket))
+    })
   }
 
   connectSocket(socket) {
@@ -21,3 +32,4 @@ class P2pSperver {
   }
 }
 
+module.exports = P2pSperver
